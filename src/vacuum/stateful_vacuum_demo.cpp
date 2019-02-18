9a96051ef1c8c4349547399fe0c16f4135014225
mtepfer <mtepfer1@gmail.com>
2019-02-18 17:08:18 -0500

    Refactored stateful vacuum demo

--- src/vacuum/stateful_vacuum_demo.cpp
+++ src/vacuum/stateful_vacuum_demo.cpp
@@ -1,21 +1,29 @@
-#include <iostream>
-
-#include <viewers/SimpleViewer.hpp>
-
+#include "viewers/SimpleViewer.hpp"
 #include "BasicVacuumEnvironment.hpp"
 #include "StatefulVacuumAgent.hpp"
+#include "stateful_vacuum_demo.hpp"
 
 using namespace aima::vacuum;
 using namespace aima::viewer;
 
+#ifndef AIMA_INTEGRATED_APP
+
+#include <iostream>
+
 int main() {
-    BasicVacuumEnvironment                                      environment;
-    StatefulVacuumAgent<environment.getX(), environment.getY()> agent;
-    SimpleViewer                                                viewer( std::cout );
+    aima::demos::stateful_vacuum_demo( std::cout );
+}
+
+#endif
+
+void aima::demos::stateful_vacuum_demo( std::ostream& out ) {
+    BasicVacuumEnvironment environment;
+    StatefulVacuumAgent    agent( environment.getX(), environment.getY());
+    SimpleViewer           viewer( std::cout );
 
     environment.addAgent( agent );
     environment.addEnvironmentView( viewer );
 
     environment.stepUntilDone();
-    environment.notifyViews( "Performance=" + std::to_string( environment.getPerformanceMeasure( agent )));
+    environment.notifyViews( "Performance=" + std::to_string( environment.getPerformanceMeasure( agent ).cleanRead()));
 }
