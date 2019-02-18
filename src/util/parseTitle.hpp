43753b24b5d7c33bb30929b16067b8ff07836240
mtepfer <mtepfer1@gmail.com>
2019-02-18 16:58:31 -0500

    Crated parseTitle utility function

---
+++ src/util/parseTitle.hpp
@@ -0,0 +1,32 @@
+#pragma once
+
+#include <string>
+#include <boost/core/demangle.hpp>
+#include <unordered_map>
+#include <typeindex>
+
+namespace aima::util {
+    template< typename T >
+    const std::string& parseTitle() {
+        static std::unordered_map<std::type_index, std::string> titles;
+
+        auto typeIndex = std::type_index( typeid( T ));
+        auto& title = titles[ typeIndex ];
+
+        if ( title.empty()) {
+            auto raw  = boost::core::demangle( typeIndex.name());
+            auto name = raw.substr( raw.find_last_of( "::" ) + 1 );
+
+            // add spaces before each capital letter except the first character
+            for ( size_t i = 1; i < name.size(); ++i )
+                if ( isupper( name[ i ] )) {
+                    name.insert( i, 1, ' ' );
+                    ++i;
+                }
+
+            title = name;
+        }
+
+        return title;
+    }
+}
