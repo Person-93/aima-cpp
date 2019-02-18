f9bb6d6055eae3715914ab5997b6ad295305ff03
mtepfer <mtepfer1@gmail.com>
2019-02-18 17:07:52 -0500

    Fixes for stateful vacuum agent
    - refactoring
    - made it work with the updated Location class

--- src/vacuum/StatefulVacuumAgent.hpp
+++ src/vacuum/StatefulVacuumAgent.hpp
@@ -3,10 +3,10 @@
 #include <functional>
 #include <sstream>
 
-#include <core/Agent.hpp>
-#include <core/Action.hpp>
-#include <core/Percept.hpp>
-
+#include "util/parseTitle.hpp"
+#include "core/Agent.hpp"
+#include "core/Action.hpp"
+#include "core/Percept.hpp"
 #include "LocalVacuumEnvironmentPercept.hpp"
 #include "BasicVacuumEnvironment.hpp"
 
@@ -15,32 +15,36 @@ namespace aima::vacuum {
         using namespace core;
     }
 
-    template< unsigned X, unsigned Y >
     class StatefulVacuumAgent : public core::Agent {
     public:
+        StatefulVacuumAgent( unsigned x, unsigned y ) : x( x ), y( y ), state( x, y ) {}
+
         const Action& execute( const Percept& percept ) override {
             auto p = dynamic_cast<const LocalVacuumEnvironmentPercept*>(&percept);
             if ( !p ) throw std::out_of_range( "Agent received percept of unrecognized type" );
             auto location = p->agentLocation;
-            state.at( location.x ).at( location.y ) = true;
-            if ( p->agentLocationState == LocationState::DIRTY ) return BasicVacuumEnvironmentBase::ACTION_SUCK;
+            state( location.x, location.y ) = true;
+            if ( p->agentLocationState == LocationState::DIRTY ) return BasicVacuumEnvironment::ACTION_SUCK;
             if ( allClean()) return Action::noOp();
-            if ( location == Location{ 0, 0 } ) return BasicVacuumEnvironmentBase::ACTION_MOVE_RIGHT;
-            if ( location == Location{ 1, 0 } ) return BasicVacuumEnvironmentBase::ACTION_MOVE_LEFT;
+            if ( location == Location{ 0, 0 } ) return BasicVacuumEnvironment::ACTION_MOVE_RIGHT;
+            if ( location == Location{ 1, 0 } ) return BasicVacuumEnvironment::ACTION_MOVE_LEFT;
             else {
                 std::ostringstream s;
-                s << percept;
-                throw std::out_of_range( "Received unrecognized percept: " + s.str());
+                s << util::parseTitle<StatefulVacuumAgent>() << " received unrecognized percept: " << percept;
+                throw std::out_of_range( s.str());
             }
         }
 
     private:
         bool allClean() {
-            return std::all_of( state.begin(), state.end(), []( const std::array<bool, Y>& array ) {
-                return std::all_of( array.begin(), array.end(), []( bool b ) { return b; } );
-            } );
+            for ( unsigned i = 0; i < x; ++i )
+                for ( unsigned j = 0; j < y; ++j )
+                    if ( !state( i, j )) return false;
+            return true;
         }
 
-        std::array<std::array<bool, Y>, X> state{ false };
+        boost::numeric::ublas::matrix<bool> state;
+        unsigned                            x;
+        unsigned                            y;
     };
 }
