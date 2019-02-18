2fbb9fec15d2d2465e36ecc534cf0ce9d21e471f
mtepfer <mtepfer1@gmail.com>
2019-02-18 16:56:47 -0500

    UniqueIdMixin - added hash and less meta-functions

--- src/util/UniqueIdMixin.hpp
+++ src/util/UniqueIdMixin.hpp
@@ -1,79 +1,91 @@
 #pragma once
 
 #include <atomic>
+#include <functional>
 
 namespace aima::util {
     template< typename T, bool visible = false >
     class UniqueIdMixin;
 
-    template< class T >
-    class UniqueIdVisibleBase;
+    namespace detail {
+        template< class T >
+        class UniqueIdVisibleBase;
 
-    template< class T >
-    class UniqueIdBase {
-        UniqueIdBase() noexcept = default;
+        template< class T >
+        class UniqueIdBase {
+            UniqueIdBase() noexcept = default;
 
-        friend class UniqueIdVisibleBase<T>;
-
-        static uint_least32_t generateId() noexcept {
-            static std::atomic_uint_least32_t counter = 0;
-            return ++counter;
-        }
-
-        uint_least32_t unique_id_ = generateId();
-
-        friend class UniqueIdMixin<T, true>;
-
-        friend class UniqueIdMixin<T, false>;
-
-        friend class UniqueIdVisibleBase<T>;
-
-    public:
-        /** A function object that can be used for sorting in maps and sets */
-        struct less {
-            bool operator()( const T& lhs, const T& rhs ) const noexcept {
-                return lhs.unique_id_ < rhs.unique_id_;
+            static uint_least32_t generateId() noexcept {
+                static std::atomic_uint_least32_t counter = 0;
+                return ++counter;
             }
+
+            uint_least32_t unique_id_ = generateId();
+
+            friend class UniqueIdMixin<T, true>;
+
+            friend class UniqueIdMixin<T, false>;
+
+            friend class UniqueIdVisibleBase<T>;
+
+        public:
+            /** A function object that can be used for sorting in maps and sets */
+            struct less {
+                bool operator()( const T& lhs, const T& rhs ) const noexcept {
+                    return lhs.unique_id_ < rhs.unique_id_;
+                }
+            };
+
+            /** A function object that can be used for hashing in unordered maps and sets */
+            struct hash {
+                std::size_t operator()( const T& t ) const noexcept {
+                    return std::hash<std::decay_t<typeof( unique_id_ )>>()( t.unique_id_ );
+                }
+            };
+
+            friend bool operator==( const T& a, const T& b ) noexcept {
+                return a.unique_id_ == b.unique_id_;
+            }
+
+            UniqueIdBase( const UniqueIdBase<T>& ) noexcept = default;
+
+            UniqueIdBase( UniqueIdBase<T>&& ) noexcept = default;
+
+            UniqueIdBase& operator=( const UniqueIdBase<T>& ) noexcept = default;
+
+            UniqueIdBase& operator=( UniqueIdBase<T>&& ) noexcept = default;
+
+            virtual ~UniqueIdBase() = default;
         };
 
-        UniqueIdBase( const UniqueIdBase<T>& ) noexcept = default;
+        template< class T >
+        class UniqueIdVisibleBase : public UniqueIdBase<T> {
+            friend class UniqueIdMixin<T, true>;
 
-        UniqueIdBase( UniqueIdBase<T>&& ) noexcept = default;
+        public:
+            /**
+             * @return The unique id
+             */
+            uint_least32_t id() const { return this->unique_id_; }
+        };
+    }
 
-        UniqueIdBase& operator=( const UniqueIdBase<T>& ) noexcept = default;
-
-        UniqueIdBase& operator=( UniqueIdBase<T>&& ) noexcept = default;
-
-        virtual ~UniqueIdBase() = default;
-    };
-
-    template< class T >
-    class UniqueIdVisibleBase : public UniqueIdBase<T> {
-        friend class UniqueIdMixin<T, true>;
-
-    public:
-        /**
-         * @return The unique id
-         */
-        uint_least32_t id() const { return this->unique_id_; }
-    };
-
-/**
- * A mixin that supplies a sorting function for use in sets and maps
- *
- * Here is an example usage.
- *
- * @code
- * class Thing : UniqueId<Thing> {};
- *
- * std::set<Thing, Thing::less> things;
- * @endcode
- *
- * @tparam T- The derived type. Uses CRTP.
- * @tparam visible- Should the unique ID be visible
- */
+    /**
+     * A mixin that supplies a sorting function for use in sets and maps
+     *
+     * Here is an example usage.
+     *
+     * @code
+     * class Thing : UniqueId<Thing> {};
+     *
+     * std::set<Thing, Thing::less> things;
+     * @endcode
+     *
+     * @tparam T- The derived type. Uses CRTP.
+     * @tparam visible- Should the unique ID be visible
+     */
     template< class T, bool visible >
-    class UniqueIdMixin : public std::conditional_t<visible, UniqueIdVisibleBase<T>, UniqueIdBase<T>> {
+    class UniqueIdMixin : public std::conditional_t<visible, detail::UniqueIdVisibleBase<T>, detail::UniqueIdBase<T>> {
         UniqueIdMixin() noexcept = default;
 
         friend T;
