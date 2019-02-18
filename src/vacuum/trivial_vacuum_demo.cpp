d90cab21bb115b34d90db07eb2082dd96bf7028c
mtepfer <mtepfer1@gmail.com>
2019-02-18 17:01:26 -0500

    Refactored trivial vacuum demo

--- src/vacuum/trivial_vacuum_demo.cpp
+++ src/vacuum/trivial_vacuum_demo.cpp
@@ -1,22 +1,30 @@
-#include <iostream>
-
 #include <viewers/SimpleViewer.hpp>
 
+#include "trivial_vacuum_demo.hpp"
 #include "BasicVacuumEnvironment.hpp"
 #include "BasicVacuumAgent.hpp"
 
-using std::cout;
 using namespace aima::vacuum;
 using aima::viewer::SimpleViewer;
 
+#ifndef AIMA_INTEGRATED_APP
+
+#include <iostream>
+
 int main() {
+    aima::demos::trivial_vacuum_demo( std::cout );
+}
+
+#endif
+
+void aima::demos::trivial_vacuum_demo( std::ostream& out ) {
     BasicVacuumEnvironment environment;
-    BasicVacuumAgent  agent;
-    SimpleViewer      viewer( cout );
+    BasicVacuumAgent       agent;
+    SimpleViewer           viewer( out );
 
     environment.addAgent( agent );
     environment.addEnvironmentView( viewer );
 
     environment.stepUntilDone();
-    environment.notifyViews( "Performance=" + std::to_string( environment.getPerformanceMeasure( agent )));
+    environment.notifyViews( "Performance=" + std::to_string( environment.getPerformanceMeasure( agent ).cleanRead()));
 }
---
+++ src/vacuum/trivial_vacuum_demo.hpp
@@ -0,0 +1,7 @@
+#pragma once
+
+#include <iosfwd>
+
+namespace aima::demos {
+    void trivial_vacuum_demo( std::ostream& out );
+}
