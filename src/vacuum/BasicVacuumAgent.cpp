603e80548a009138c9a2839b6596e0144921322a
mtepfer <mtepfer1@gmail.com>
2019-02-18 17:18:43 -0500

    Updated Basic Vacuum Agent to be compatilbe with updated Vacuum classes

--- src/vacuum/BasicVacuumAgent.cpp
+++ src/vacuum/BasicVacuumAgent.cpp
@@ -1,5 +1,5 @@
-#include <core/Action.hpp>
-
+#include "util/parseTitle.hpp"
+#include "core/Action.hpp"
 #include "BasicVacuumAgent.hpp"
 #include "LocalVacuumEnvironmentPercept.hpp"
 #include "BasicVacuumEnvironment.hpp"
@@ -8,11 +8,15 @@ using namespace aima::core;
 
 namespace aima::vacuum {
     const Action& BasicVacuumAgent::execute( const Percept& percept ) {
-        auto p = dynamic_cast<const LocalVacuumEnvironmentPercept*>(&percept);
-        if ( !p ) throw std::out_of_range( "Agent received percept of unrecognized type" );
-        if ( p->agentLocationState == LocationState::DIRTY ) return BasicVacuumEnvironmentBase::ACTION_SUCK;
-        if ( p->agentLocation == Location{ 0, 0 } ) return BasicVacuumEnvironmentBase::ACTION_MOVE_RIGHT;
-        if ( p->agentLocation == Location{ 1, 0 } ) return BasicVacuumEnvironmentBase::ACTION_MOVE_LEFT;
+        auto               p = dynamic_cast<const LocalVacuumEnvironmentPercept*>(&percept);
+        std::ostringstream s;
+        if ( !p ) {
+            s << util::parseTitle<BasicVacuumAgent>() << " received percept of unrecognized type";
+            throw std::out_of_range( s.str());
+        }
+        if ( p->agentLocationState == LocationState::DIRTY ) return BasicVacuumEnvironment::ACTION_SUCK;
+        if ( p->agentLocation == Location{ 0, 0 } ) return BasicVacuumEnvironment::ACTION_MOVE_RIGHT;
+        if ( p->agentLocation == Location{ 1, 0 } ) return BasicVacuumEnvironment::ACTION_MOVE_LEFT;
         return Action::noOp();
     }
 }
