a1bd2f4ba270156e65349d4921b5420e5d28d313
mtepfer <mtepfer1@gmail.com>
2019-07-21 14:54:27 -0400

    SearchNode - deleted copy and move constructors

--- src/PathFinder/SearchNode.hpp
+++ src/PathFinder/SearchNode.hpp
@@ -15,7 +15,9 @@ namespace aima::path_finder {
         SearchNode( std::weak_ptr<SearchNode> parent, Point location, float pathCost ) :
                 parent{ std::move( parent ) }, location{ location }, pathCost{ pathCost } {}
 
-        SearchNode( const SearchNode& ) = default;
+        SearchNode( const SearchNode& ) = delete;
+
+        SearchNode( SearchNode&& ) = delete;
 
         const std::shared_ptr<SearchNode>& addChild( std::shared_ptr<SearchNode> child ) {
             return *children.emplace( std::move( child )).first;
