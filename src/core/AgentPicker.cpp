12da14dd1f9a46e48c3a924b2d35a551df69f96e
mtepfer <mtepfer1@gmail.com>
2019-08-01 18:32:24 -0400

    AgentPicker - added option for single selection only

--- src/core/AgentPicker.cpp
+++ src/core/AgentPicker.cpp
@@ -5,26 +5,36 @@
 
 using aima::core::detail::AgentPickerBase;
 
-AgentPickerBase::AgentPickerBase( std::string title, bool* stayOpen ) :
+AgentPickerBase::AgentPickerBase( std::string title, bool* stayOpen, bool multiSelect ) :
         windowConfig{ std::make_unique<gui::WindowConfig>( gui::WindowConfig{
                 .title =  std::move( title ),
                 .open  = stayOpen,
-        } ) } {}
+        } ) },
+        multiSelect{ multiSelect } {}
 
 bool AgentPickerBase::render( aima::gui::ImGuiWrapper& imGuiWrapper ) {
     return imGuiWrapper.window( *windowConfig, [ & ]() {
-        for ( auto&[name, value] : selections ) {
-            ImGui::Checkbox( name.data(), &value );
-        }
-
-        auto disable = imGuiWrapper.disableControls( selectionsComplete );
-        if ( ImGui::Button( "OK" )) {
+        const auto onComplete = [ this ] {
             selectionsComplete = true;
             std::thread( [ this ]() {
                 finalize();
                 agentsCreated = true;
             } ).detach();
+        };
+
+        if ( multiSelect ) {
+            for ( auto&[name, value] : selections )
+                ImGui::Checkbox( name.data(), &value );
+
+            auto disable = imGuiWrapper.disableControls( selectionsComplete );
+            if ( ImGui::Button( "OK" ))
+                onComplete();
         }
+        else
+            for ( auto&[name, value] : selections )
+                if ( ImGui::Selectable( name.begin(), &value ))
+                    onComplete();
+
         return true;
     } );
 }
--- src/core/AgentPicker.hpp
+++ src/core/AgentPicker.hpp
@@ -1,7 +1,6 @@
-#include <utility>
-
 #pragma once
 
+#include <utility>
 #include <memory>
 #include <vector>
 #include <algorithm>
@@ -24,7 +23,7 @@ namespace aima::core {
             bool done() const { return agentsCreated; }
 
         protected:
-            AgentPickerBase( std::string title, bool* stayOpen );
+            AgentPickerBase( std::string title, bool* stayOpen, bool multiSelect );
 
             virtual void finalize() = 0;
 
@@ -34,6 +33,7 @@ namespace aima::core {
         private:
             bool selectionsComplete{ false };
             bool agentsCreated{ false };
+            bool multiSelect;
         };
     }
 
@@ -42,20 +42,28 @@ namespace aima::core {
     public:
         using Agents = std::vector<std::unique_ptr<Base>>;
 
-        AgentPicker( Environment& environment, bool* stayOpen ) :
-                AgentPickerBase{ "Choose " + std::string{ util::parseTitle<Base>() } + "s", stayOpen },
+        AgentPicker( Environment& environment, bool* stayOpen, bool multiSelect = true ) :
+                AgentPickerBase{ "Choose " + std::string{ util::parseTitle<Base>() } + "s", stayOpen, multiSelect },
                 environment{ environment } {
-            auto agentTypes = detail::agentRegistry().at( typeid( Base ));
+            auto agentTypes = detail::agentRegistry()[ typeid( Base ) ];
             for ( auto[name, _]: agentTypes ) {
                 selections.insert( { name, false } );
             }
         }
 
+        template< typename Function >
+        AgentPicker( Environment& environment, bool* stayOpen, Function function, bool multiSelect = true ) :
+                AgentPicker( environment, stayOpen, multiSelect ) {
+            processAgent = std::move( function );
+        }
+
+    protected:
         void finalize() override {
             for ( auto&[name, value] : selections ) {
                 if ( value ) {
                     auto p = makeAgent<Base>( name );
                     environment.addAgent( *p );
+                    if ( processAgent ) processAgent( *p );
                     agents.push_back( std::move( p ));
                 }
             }
@@ -63,6 +71,7 @@ namespace aima::core {
 
     private:
         Agents agents;
-        Environment& environment;
+        Environment                  & environment;
+        std::function<void( Base& )> processAgent;
     };
 }
